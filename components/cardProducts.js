a6e4fe678a53442bf055f1e2ac3020db08e4838e
LeanNaikim <leangnaikim168@gmail.com>
2024-02-28 10:31:57 +0700

    Update images

--- components/cardProducts.js
+++ components/cardProducts.js
@@ -64,9 +64,9 @@ export const cardUser = (user) => {
         </div>
     </div>
     <div class="flex flex-col items-center pb-10">
-        <img class="w-24 h-24 mb-3 rounded-full shadow-lg" src="https://cdn-icons-png.flaticon.com/512/10539/10539295.png" alt="Bonnie image"/>
+        <img class="w-24 h-24 mb-3 rounded-full shadow-lg" src="${user?.image || CardProductPlaceholder}" alt="Bonnie image"/>
         <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">${user?.name || "UnKnown"}</h5>
-        <span class="text-sm text-gray-500 dark:text-gray-400">${user?.code || "Unvarialable"}</span>
+        <span class="text-sm text-gray-500 dark:text-gray-400">code: ${user?.code || "Unvarialable"}</span>
         <div class="flex mt-4 md:mt-6">
             <a href="#" class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add friend</a>
             <a href="#" class="py-2 px-4 ms-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Message</a>
